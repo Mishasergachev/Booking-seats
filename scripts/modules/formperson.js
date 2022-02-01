7172d038aae3b58225fee1ae85ff79ae9be46c17
0000 <a0000@MacBook-Air-0000.local>
2022-02-01 14:08:38 +0100

    Update formperson.js

--- scripts/modules/formperson.js
+++ scripts/modules/formperson.js
@@ -29,13 +29,35 @@ const createFormPerson = (n) => {
   });
   fieldName.append(labelName, inputName);
 
+  const fieldTicket = createElement("div", {
+    className: "field",
+  });
+
+  const labelTicket = createElement("label", {
+    className: "field__label",
+    textContent: "Номер билета (10 цифр)",
+  });
+
+  const inputTicket = createElement("input", {
+    className: "field__input",
+    id: "ticket0",
+    name: "ticket",
+    type: "text",
+    placeholder: "Номер билета",
+    required: true,
+    minLength: "10",
+    maxLength: "10",
+  });
+
+  fieldTicket.append(labelTicket, inputTicket);
+
   const button = createElement("button", {
     className: "btn-confirm",
     type: "submit",
     textContent: "Confirm",
   });
 
-  form.append(title, fieldName, button);
+  form.append(title, fieldName, fieldTicket, button);
 
   return form;
 };
