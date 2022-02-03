c6c81ab8293b450c44625bc0ceb93fe9a0b8f329
0000 <mikhail.sergachev@hyperisland.se>
2022-02-03 08:29:34 +0100

    serv

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 import start from "./modules/start.js";
 import getFormPerson from "./modules/formperson.js";
 import readyPlane from "./modules/readyPlane.js";
+import getData from './service/getTour.js'
 const init = (selectorApp, title) => {
   const app = document.querySelector(selectorApp);
 
